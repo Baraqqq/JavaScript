2e4587e1b3136db6d1f14f79b09a682c34c7d050
Ilias <iliasbussnis@gmail.com>
2022-02-08 09:54:18 +0100

    Week 1 - Ontwikkelaars 2
    JS Week 1 - Ontwikkelaars 2

---
+++ week1/Ontwikkelaars-2.js
@@ -0,0 +1,11 @@
+const age = 12;
+if(true){
+    case age >= 18:
+        console.log("Great!");
+        break;
+    case age >= 12:
+        console.log("Almost great");
+        break;
+    default:
+        console.log("Not great");
+}
